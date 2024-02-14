52100f098f974ecdbcb23ae93b9d27ef6de08eba
pimasterpi <lucas.indy@gmail.com>
2024-02-13 18:20:33 -0800

    Create Person Class

---
+++ back_end/Person.java
@@ -0,0 +1,112 @@
+public class Person {
+
+    public enum Gender {
+        MALE,
+        FEMALE,
+        NONBINARY
+    }
+
+    public enum Role {
+        RA,
+        REA,
+        REC
+    }
+
+    public enum Hall {
+
+        /* All Male */
+        CARY,
+        MCCUTCHEON,
+        TARKINGTON,
+
+        /* All Female */
+        MEREDITH,
+        MEREDITH_SOUTH,
+        WINDSOR,
+
+        /* Co-Ed */
+        EARHART,
+        FIRST_STREET_TOWERS,
+        FRIEDA_PARKER_HALL,
+        WINIFRED_PARKER_HALL,
+        HARRISON_HALL,
+        HAWKINGS_HALL,
+        HILLENBRAND_HALL,
+        HONORS_COLLEGE,
+        OWEN_HALL,
+        SHREVE_HALL,
+        WILEY
+    }
+
+    private String name = null;
+    private String email = null;
+    private String password = null;
+    private int[10] puid = null;
+    private Gender gender = null;
+    private Role role = null;
+    private Hall hall = null;
+    private enabled = false;
+
+    /* Default Constructor */
+    public Person() {}
+
+    /* Constructor to  */
+    public Person(String name, String email, String password, Gender gender, Role role, Hall hall) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.gender = gender;
+        this.role = role;
+        this.hall = hall;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Gender getGender() {
+        return gender;
+    }
+
+    public void setGender(Gender gender) {
+        this.gender = gender;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    public Hall getHall() {
+        return hall;
+    }
+
+    public void setHall(Hall hall) {
+        this.hall = hall;
+    }
+
+
+}

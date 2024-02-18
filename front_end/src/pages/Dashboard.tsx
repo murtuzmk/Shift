895c95ba8e78c8a477dc9d96bd2320d04f61808c
Daniel Fakunle <tosinfakunle@yahoo.com>
2024-02-18 12:58:50 -0500

    Implemented Log in and Log out with Auth0

--- front_end/src/pages/Dashboard.tsx
+++ front_end/src/pages/Dashboard.tsx
@@ -1,8 +1,20 @@
+import { useUser } from "../hooks/useUser";
+
 // This is the overview page where user can
 export const Dashboard = () => {
+  const { user } = useUser();
+
   return (
     <div className="text-2xl font-semibold flex justify-center items-center h-full">
-      Dashboard
+      {user ? (
+        <div>
+          <img src={user.picture} alt={user.name} />
+          <h2>Username: {user.name}</h2>
+          <p>Email: {user.email}</p>
+        </div>
+      ) : (
+        <h1>Dashboard</h1>
+      )}
     </div>
   );
 };

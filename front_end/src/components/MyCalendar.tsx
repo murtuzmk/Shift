f77788f0e50cb7f2d8bba1c0b37beb4b4f533df4
DanChigo <111812010+DanChigo@users.noreply.github.com>
2024-02-21 21:29:48 -0500

    Calendar Component made

---
+++ front_end/src/components/MyCalendar.tsx
@@ -0,0 +1,34 @@
+import { Calendar, momentLocalizer } from 'react-big-calendar';
+import moment from 'moment';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+
+const localizer = momentLocalizer(moment);
+
+const events = [
+  {
+    title: 'Event 1',
+    start: new Date(),
+    end: new Date(),
+  },
+  {
+    title: 'Event 2',
+    start: new Date(),
+    end: new Date(),
+  },
+];
+
+const MyCalendar = () => {
+  return (
+    <div style={{ height: '500px' }}>
+      <Calendar
+        localizer={localizer}
+        events={events}
+        startAccessor="start"
+        endAccessor="end"
+        style={{ margin: '50px' }}
+      />
+    </div>
+  );
+};
+
+export default MyCalendar;

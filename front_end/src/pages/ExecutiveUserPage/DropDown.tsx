ec0490fa0ca197928fd2c5435a069f2bd1db874d
Samson Tesfagiorgis <stesfag@purdue.edu>
2024-02-26 12:19:48 -0500

    testing

--- front_end/src/pages/ExecutiveUserPage/DropDown.tsx
+++ front_end/src/pages/ExecutiveUserPage/DropDown.tsx
@@ -1,21 +1,15 @@
-function Dropdown({
-  options,
-  onSelect,
-}: {
-  options: any[];
-  onSelect: (value: any) => void;
-}) {
-  return (
-    <select
-      onChange={onSelect}
-      className="border border-gray-300 rounded-md text-gray-700 py-2 px-4"
-    >
-      {options.map((option, index) => (
-        <option key={index} value={option.value}>
-          {option.label}
-        </option>
-      ))}
-    </select>
-  );
+function Dropdown({ options, onSelect }: { options: any[], onSelect: (value: any) => void }) {
+    return (
+        <select
+            onChange={onSelect}
+            className="border border-gray-300 rounded-md text-gray-700 py-2 px-4"
+        >
+            {options.map((option, index) => (
+                <option key={index} value={option.value}>
+                    {option.label}
+                </option>
+            ))}
+        </select>
+    );
 }
 export default Dropdown;

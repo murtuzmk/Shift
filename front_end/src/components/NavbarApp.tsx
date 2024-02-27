2f0662098e2d73e9d00e5d78a388fdccfb6ddadc
Daniel Fakunle <tosinfakunle@yahoo.com>
2024-02-26 23:18:36 -0500

    Styled Navbar and Sidebar

--- front_end/src/components/NavbarApp.tsx
+++ front_end/src/components/NavbarApp.tsx
@@ -1,10 +1,14 @@
 import { useAuth0 } from "@auth0/auth0-react";
+import { Icon } from "@chakra-ui/react";
+import { HiMiniBars3 } from "react-icons/hi2";
 
 export const NavbarApp = () => {
   const { logout } = useAuth0();
   return (
-    <header className="bg-gray-500 py-5 flex justify-center gap-x-6">
-      <h1 className="text-2xl font-semibold">NavbarApp</h1>
+    <header className="bg-gray-50 py-3 px-5 flex justify-between gap-x-6 border-b-2 border-gray-300">
+      <div className="hover:bg-gray-200 p-2 rounded-md">
+        <Icon as={HiMiniBars3} w={6} h={6} />
+      </div>
       <button
         onClick={() =>
           logout({ logoutParams: { returnTo: window.location.origin } })

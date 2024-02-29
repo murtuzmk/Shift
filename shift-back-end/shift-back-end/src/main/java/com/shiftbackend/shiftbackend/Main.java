99080ba5cf4034579fb06917194ffbe9eb0bc770
pimasterpi <57331189+pimasterpi@users.noreply.github.com>
2024-02-29 09:04:08 -0800

    Initialize Spring Boot

---
+++ shift-back-end/shift-back-end/src/main/java/com/shiftbackend/shiftbackend/Main.java
@@ -0,0 +1,37 @@
+package com.shiftbackend.shiftbackend;
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        /* Person person = new Person();
+        person.setTimezone(1);
+        System.out.println(person.toString());
+        TimeBlock time = new TimeBlock(17, 37, 2, 17, 2024, 0);
+        System.out.println(time.toString());
+        System.out.println(time.dayOfWeek());
+        System.out.println(time.isExpired(0, 0, 0));
+        time = new TimeBlock(9, 37, 2, 17, 2024, -8);
+        System.out.println(time.toString());
+        System.out.println(time.dayOfWeek());
+        System.out.println(time.isExpired(0, 0, 0));
+        */
+
+        ResidentAssistant ra = new ResidentAssistant("Alice", "dummy@gmail.com", "0123456789",
+                Person.Gender.FEMALE, Person.Hall.CARY, true, "1A", false, null, null);
+        ResidentEducationAssistant rea = new ResidentEducationAssistant("Bob", "dummy@gmail.com",  "1234567890",
+                Person.Gender.MALE, Person.Hall.CARY, true, "1A", false, null, null, null, null);
+        ResidentEducationAssistant rec = new ResidentEducationCoordinator("Cab", "dummy@gmail.com", "2345678901",
+                Person.Gender.NONBINARY, Person.Hall.CARY, true, "1A", false, null, null, null, null, null);
+
+        ResidentAssistant person = new ResidentAssistant();
+        System.out.println( person.loadAccountFile("0123456789"));
+
+        ra.saveAccountFile();
+        //ra.deleteAccountFile();
+        //rea.saveAccountFile();
+        //rea.deleteAccountFile();
+        //rec.saveAccountFile();
+        //rec.deleteAccountFile();
+    }
+}

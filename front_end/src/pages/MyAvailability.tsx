bacedfe57d7836ca089f6a5e0a3b597619cb9f16
Samson Tesfagiorgis <stesfag@purdue.edu>
2024-02-29 21:16:49 -0500

    Added Button Functionality and User Availability Page with functionality

--- front_end/src/pages/MyAvailability.tsx
+++ front_end/src/pages/MyAvailability.tsx
@@ -4,7 +4,7 @@ import MyCalendar from "../components/MyCalendar";
 export const MyAvailability = () => {
   return (
     <div className="space-y-5">
-      <h1 className="text-center font-bold"> My Availability</h1>
+      <h1 className="text-center font-bold">Availability</h1>
       <div>
         <AvailabilityCalendar />
       </div>

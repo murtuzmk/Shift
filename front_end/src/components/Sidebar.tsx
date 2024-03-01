70e9baaa3995616fc850d93b41438d4acbe8ccc6
Samson Tesfagiorgis <stesfag@purdue.edu>
2024-02-29 22:43:00 -0500

    Button functionality added on sidebar

--- front_end/src/components/Sidebar.tsx
+++ front_end/src/components/Sidebar.tsx
@@ -3,7 +3,6 @@ import { FaGear } from "react-icons/fa6";
 import { HiCalendar, HiMiniInboxArrowDown } from "react-icons/hi2";
 import { MdSpaceDashboard } from "react-icons/md";
 import { useNavigate } from "react-router-dom";
-
 const Sidebar = () => {
   const navigate = useNavigate();
   return (
@@ -35,7 +34,21 @@ const Sidebar = () => {
           role="group"
           onClick={() => navigate("availability")}
         >
-          Schedule
+          Availability
+        </Button>
+        <Button
+          leftIcon={
+            <Icon
+              as={HiMiniInboxArrowDown}
+              className="size-6 fill-gray-500 !w-5 !h-5 mr-2"
+              _groupHover={{ fill: "gray.600" }}
+            />
+          }
+          className="!bg-gray-50 hover:!bg-gray-200 focus:ring-4 !font-medium !text-base !px-3 !justify-start"
+          role="group"
+          onClick={() => navigate("executivepage")}
+        >
+          Executive
         </Button>
         <Button
           leftIcon={

44abbd92428cf5df56ce01054d4aa574a9b63157
Samson Tesfagiorgis <Samson@pal-nat186-8-223.itap.purdue.edu>
2024-03-26 21:49:44 -0400

    Added more fetch requests fixed formatting of execPage

--- front-end/src/components/AvailabilityCalendar.tsx
+++ front-end/src/components/AvailabilityCalendar.tsx
@@ -14,7 +14,6 @@ interface TileClassNameArgs {
 const AvailabilityCalendar: React.FC<{ id: string | null, execAccess : boolean | null}> = ({id, execAccess}) => {
   // Explicitly type freeDays as an array of strings
   const [freeDays, setFreeDays] = useState<string[]>([]);
-  const [savedFreeDays, setSavedFreeDays] = useState<string[]>([]);
   const [currentMonth, setCurrentMonth] = useState(new Date());
   const [view, setView] = useState<"month">("month");
   const {user} = useAuth0();
@@ -27,7 +26,7 @@ const AvailabilityCalendar: React.FC<{ id: string | null, execAccess : boolean |
   /* Fetch the free days from the REST API */
   const fetchFreeDays = () => {
     // Update the freeDays state with the fetched data
-    fetch("http://localhost:8080/root/${userid}/get-preferences")
+    fetch("http://localhost:8080/ra/${userid}/get-preferences")
     .then((response) => response.json())
     .then((data) => {
       const dateStr: string= new Date(data).toISOString().split("T")[0];
@@ -35,8 +34,9 @@ const AvailabilityCalendar: React.FC<{ id: string | null, execAccess : boolean |
     });
   }
 
-  const addFreeDays = (dates : string[]) => {
-    fetch("http://localhost:8080/root/${userid}/add-preference", {
+  /* This is for when submit is enacted, tells us which days are left to be saved */
+  const setSavedFreeDays = (dates : string[]) => {
+    fetch("http://localhost:8080/ra/${userid}/add-preference", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
@@ -51,6 +51,7 @@ const AvailabilityCalendar: React.FC<{ id: string | null, execAccess : boolean |
       setFreeDays([...freeDays, ...dates]); // Append the new dates to the existing ones
     });
   }
+  
 
 
   /* useEffect will call on fetch free days once, when the component is mounted */
@@ -137,7 +138,7 @@ const AvailabilityCalendar: React.FC<{ id: string | null, execAccess : boolean |
     /*localStorage.setItem("savedFreeDays", JSON.stringify(freeDays));
     setSavedFreeDays(freeDays);
     console.log(freeDays);*/
-    addFreeDays(freeDays);
+    setSavedFreeDays(freeDays);
     window.alert("Availability Successfully Sent Out");
   };
 
